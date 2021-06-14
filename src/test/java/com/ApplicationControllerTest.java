ab894c60d948f77c12d0b00e0a636579ba087b12
q48077 <baskar.kumarasamy@delta.com>
2021-06-14 15:56:07 -0400

    buildspec.yml added

---
+++ src/test/java/com/ApplicationControllerTest.java
@@ -0,0 +1,39 @@
+package com;
+
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ApplicationControllerTest.class)
+@ActiveProfiles("test")
+class ApplicationControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @InjectMocks
+    private ApplicationController applicationController;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.mvc =  MockMvcBuilders.standaloneSetup(applicationController).build();
+    }
+
+    @Test
+    public void testContextLoads(){
+        assertEquals("Hello JUnit 5", "Hello JUnit 5");
+    }
+
+}
